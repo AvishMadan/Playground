57f2e11686343ea6228aaa90530de2e254a904fc
AvishMadan <40319707+AvishMadan@users.noreply.github.com>
2018-06-16 20:03:10 +0530

    Update RandomNUm.java

--- src/simplejava/RandomNUm.java
+++ src/simplejava/RandomNUm.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package simplejava;
 
 import java.util.Random;
@@ -28,33 +27,3 @@ public class RandomNUm {
 		
 	}
 }
-=======
-package simplejava;
-
-import java.util.Random;
-
-/**
-@author Avish Madan
-
-**/
-public class RandomNUm {
-
-	
-	public static void main(String[] args) {
-		
-		int max=100;
-		int min=0;
-		Random random=new Random()
-				
-				;
-		for(int i=0;i<20;i++){
-		int num=min+random.nextInt(max);
-		
-		System.out.println(num);
-		
-		}
-		
-		
-	}
-}
->>>>>>> branch 'master' of https://github.com/AvishMadan/Playground.git
